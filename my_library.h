d5eaa9ef0285df3bbaf1792f641b9e4b5a7a7d0d
Blattstein <arnas.lukosius@outlook.com>
2025-05-25 15:30:10 +0300

    Create my_library.h

---
+++ my_library.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+// -- <iostream>
+using std::cout;
+using std::endl;
+using std::cerr;
+
+// -- <fstream>
+using std::ifstream;
+
+// -- <sstream>
+using std::istringstream;
+
+// -- <string>
+using std::string;
+using std::ios;
+
+// -- <vector
+using std::vector;
