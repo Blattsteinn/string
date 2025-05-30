90af41d174dae2da749cea80b281017e36224670
Blattstein <arnas.lukosius@outlook.com>
2025-05-30 20:54:27 +0300

    Update my_functions.h

--- code/my_functions.h
+++ code/my_functions.h
@@ -1,7 +1,6 @@
-#include "my_library.h"
+#pragma once
 
 string readFileToString(const string &file_name);
 vector<string> splitIntoWords(const string &text);
 vector<string> splitIntoWords(const string &text);
-bool getLine(const std::string& filename, int targetLine, string& out);
 void print(const unordered_map<string, wordDetails>& word_information);
